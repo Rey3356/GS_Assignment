201b5499188b3b6eb3c9e976a5e9d609afd500f2
Rey3356 <aayushsoni92@gmail.com>
2023-01-16 13:44:28 +0530

    Created Scriptable Object
    Created Scriptable Object with List of type S_Class. Created asset of ScOb in project.

---
+++ Assets/Scripts/Sc_Ob.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScOb", menuName = "ScOb")]
+public class Sc_Ob : ScriptableObject
+{
+    public List<S_Class> TheList;
+}
