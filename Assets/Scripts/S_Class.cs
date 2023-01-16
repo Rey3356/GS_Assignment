1867778c196cf55bc0a65dd61b7de022a35ea622
Rey3356 <aayushsoni92@gmail.com>
2023-01-16 13:27:19 +0530

    creating scripts folder and first script
    Creating "SClass" Serializable class with needed components

---
+++ Assets/Scripts/S_Class.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Creates a Serializable class with an image component, a vector2 field, and an int value.
+[System.Serializable]
+public class S_Class
+{
+    public Sprite image;
+    public Vector2 vec_2;
+    public int IntEger;
+}
